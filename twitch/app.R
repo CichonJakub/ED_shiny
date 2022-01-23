953eb92ad38bb1dd75899be6f4be1e6b1b7e5190
awro1444 <57668665+awro1444@users.noreply.github.com>
2022-01-23 19:32:24 +0100

    wstepne kminienie2

--- twitch/app.R
+++ twitch/app.R
@@ -8,6 +8,14 @@
 #
 
 library(shiny)
+library(readr)
+library(readxl)
+library(dplyr)
+library(tidyverse)
+library(ggpubr)
+
+Twitch_global_data_1 <- read_csv("Twitch_global_data (1).csv")
+View(Twitch_global_data_1)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -20,9 +28,9 @@ ui <- fluidPage(
         sidebarPanel(
             sliderInput("bins",
                         "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+                        min = 2016,
+                        max = 2021,
+                        value = 2018)
         ),
 
         # Show a plot of the generated distribution
@@ -34,14 +42,23 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+    
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        # x    <- faithful[, 2]
+        # bins <- seq(min(x), max(x), length.out = input$bins)
+        bins <- input$bins
+        
+        # Twitch_global_data_per_year <- Twitch_global_data_1 %>% subset(year == bins)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        #hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        
+        ggplot(subset(Twitch_global_data_1, year == bins), aes(x=Month, y=`Streams`)) + geom_col(fill="#debb2f") + 
+          labs(title = "Sumaryczna powierzchnia gruntów w poszczególnych województwach", x = "Województwo", y = "Powierzchnia [ha]")
+        
     })
 }
 
